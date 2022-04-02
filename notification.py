61fe0106083eff1e945ece931cf6aeac8d1b79db
Zachery T. Eritano <60812567+DexeHecimal@users.noreply.github.com>
2022-04-02 01:49:09 -0400

    Update notification.py
    Refined the comments and refurbished the example values to make them easier for users t understand.

--- notification.py
+++ notification.py
@@ -1,52 +1,55 @@
-# Basic bandiwth montior/attack capture & discord report webhook created by (Zachery T. Eritano [12/1/2021]).
+# Sends information to Discord webhooks regarding the possible DDoS attack(s) that has/have been detected.
+# Created by: Zachery T. Eritano, Date: 4/2/2022
+
+# Import required modules for our script.
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import sys
 
-# received passed attack information.
+# Recieve information passed by "capture.sh" as system arguments to be referenced.
 pps = sys.argv[1]
 cpu = sys.argv[2]
 mbps = sys.argv[3]
 pcapdir = sys.argv[4]
 pcap = sys.argv[5]
 
-# place your discord web hook here.
+# Place your Discord webhook for your embed's here.
 webhook = DiscordWebhook(url='')
 
-# pcap files output through this web hook here. I didn't want the public to have access to server captures, so I had it delivered to a private channel.
+# Place the Discord webhook for the channel in which you'd like your pcap files to be sent to.
 webhook2 = DiscordWebhook(url='')
 
-# creating the discord webhook, you can customize this how ever you please.
-embed = DiscordEmbed(title='Attack Detected', description='A possible attack has been detected against this network service.', url="https://rapidz.xyz/attacks", color='0xff0000')
+# Creating the embed to be sent to the specified discord webhook.
+embed = DiscordEmbed(title='Attack(s) Detected!', description="We've detected a possible DDoS!", url="https://example.com", color='0xff0000')
 # embed.set_author(name='Network Logs', url='https://rapidz.xyz/attacks', icon_url='https://cdn.discordapp.com/attachments/915134700094378055/915448421895270440/server-icon.png')
 
-# customize embed fields. (information about the service under attack.)
-embed.add_embed_field(name="Location:", value="Canada, Montreal", inline=True)
-embed.add_embed_field(name="Provider:", value="100UP Hosting", inline=True)
-embed.add_embed_field(name="Address:", value="149.56.35.x/32", inline=False)
-embed.add_embed_field(name="Capacity:", value="4 Terabits(s) +", inline=False)
+# Customize embed fields, this is purely design.
+embed.add_embed_field(name="Location:", value="Country, State", inline=True)
+embed.add_embed_field(name="Provider:", value="ISP/Provider", inline=True)
+embed.add_embed_field(name="Address:", value="IP Address(s)", inline=False)
 
-# refernce the passed attributes of the attack via system args.
+# Show information about bandwidth and CPU usage during the detected attack.
 embed.add_embed_field(name="Megabits(s):", value=mbps, inline=True)
 embed.add_embed_field(name="Packets(s):", value=pps, inline=True)
 embed.add_embed_field(name="CPU Usage:", value=f"{cpu} %", inline=True)
 
-# set embed thumbnail. (change this if you'd like.)
-embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/915134700094378055/915449588234723348/canada-flag-png-large.jpeg')
+# Set embed thumbnail, this is purely design.
+embed.set_thumbnail(url='https://example.com/example.png')
 
-# set embed image. (change this if you'd like.)
-embed.set_image(url='https://cdn.discordapp.com/attachments/915134700094378055/915452127294418954/standard.gif')
+# Set embed image, this is purely design.
+embed.set_image(url='https://example.com/example.png')
 
-# change the footer at the bottom of the embed. icon_url for the image next to the footer.
-embed.set_footer(text='The attack has been automatically dumped & reported to the discord!', icon_url="https://cdn.discordapp.com/attachments/912441302568828988/912556833426800700/Warning.svg.png")
+# Footer text an image, This is purely deisgn. Content up to user discretion.
+embed.set_footer(text='Automated packet capture initiated, our mitigation system is currently trying to mitigate the attack(s)!', icon_url="https://example.com/example.png")
 
-# set the first webhook's embed.
+# Create the embed for our Discord webhook.
 webhook.add_embed(embed)
-# executing the first webhook.
+
+# Send our response for the Discord webhook for our embed.
 response = webhook.execute()
 
-# locate and send the pcap file that's been captured to our private channel.
+# Locate the file of the captured attack, then send it to our second Discord webhook.
 with open(f"{pcapdir}/capture.{pcap}.pcap", "rb") as f:
-    webhook2.add_file(file=f.read(), filename=f"canada-1.dump.{pcap}.pcap")
+    webhook2.add_file(file=f.read(), filename=f"example-1.dump.{pcap}.pcap")
 
-# seperate webhook for private access only to the file uploaded.
+# Send our response for the Discord webhook for our ".pcap" file.
 response = webhook2.execute()
